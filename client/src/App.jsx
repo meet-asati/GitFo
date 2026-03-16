52d82d4161abc564ee83152012dbb8b55e0e844e
meetasati07 <mmasati1976@gmail.com>
2026-03-16 22:50:13 +0530

    Client Server connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react"
+
 function App() {
 
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('/api/msg').then((data) => {
+      setMessage(data.data.message)
+    });
+  }, [])
+
   return (
-    <>
-      <div className="text-center">GitFo</div>
-    </>
+    <div className="text-center">
+      <div>GitFo</div>
+      <div>
+        {
+          message == '' ?
+            <p>Loading...</p>
+            : <p>{message}</p>
+        }
+      </div>
+
+    </div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => {
-  res.end("Welcome to GitFo");
+app.use(cors());
+app.use(express.json())
+
+app.get("/api/msg", (req, res) => {
+  res.json({ message: "Welcome to GitFo" });
 });
 
 app.listen(PORT, () => {
