44d6f0421c771bb29586f5c22f6468851414aec9
meetasati07 <mmasati1976@gmail.com>
2026-03-17 22:24:45 +0530

    Hero Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react"
 import NavBar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 function App() {
 
   return (
     <div className="flex flex-col justify-center bg-[#181818] h-screen">
-      <div className="h-screen mx-48 my-10">
+      <div className="flex flex-col gap-20 h-screen mx-48 my-10">
         <NavBar />
+        <HeroSection />
       </div>
     </div>
   )
---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import chart from '../assets/green_charts.png'
+
+function HeroSection() {
+    return (
+        <div className="flex flex-col gap-10 items-center bg-black rounded-2xl h-120 p-10 text-center">
+            <h1 className="text-white text-3xl">Decode Your Repositories. Drive Better Development.</h1>
+            <h2 className="text-2xl text-[#8C8C8C]">GitFo transforms raw GitHub data into actionable insights. Track contributor impact, visualize repository health, and optimize your workflow with a single, powerful dashboard.</h2>
+        </div>
+    )
+}
+
+export default HeroSection;
