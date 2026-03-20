e3dfc42ad3db54ecad41245b9a50dedfe7a5dcf4
meetasati07 <mmasati1976@gmail.com>
2026-03-20 17:09:16 +0530

    feat: start decoding button and routing

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+
 function HeroSection() {
+
+    const navigate = useNavigate();
+
+    function handleButton() {
+        navigate(`/signup`)
+    }
+
     return (
-        <div className="flex flex-col gap-10 items-center bg-black rounded-2xl h-120 p-10 text-center">
-            <h1 className="text-white text-3xl">Decode Your Repositories. Drive Better Development.</h1>
-            <h2 className="text-2xl text-[#8C8C8C]">GitFo transforms raw GitHub data into actionable insights. Track contributor impact, visualize repository health, and optimize your workflow with a single, powerful dashboard.</h2>
+        <div className="flex flex-col gap-20 items-center bg-black rounded-2xl h-120 p-10">
+            <div className="flex flex-col gap-10 p-10 text-center">
+                <h1 className="text-white text-3xl">Decode Your Repositories. Drive Better Development.</h1>
+                <h2 className="text-2xl text-[#8C8C8C]">GitFo transforms raw GitHub data into actionable insights. Track contributor impact, visualize repository health, and optimize your workflow with a single, powerful dashboard.</h2>
+            </div>
+            <button onClick={handleButton} className="text-white text-4xl bg-[#2C2C2C] p-6 border-white border-2 rounded-4xl">Start Decoding</button>
         </div>
     )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,10 @@ function NavBar() {
 
     const navigate = useNavigate();
 
+    function handleLanding() {
+        navigate(`/`)
+    }
+
     function handleLogin() {
         navigate(`/login`)
     }
@@ -14,7 +18,7 @@ function NavBar() {
 
     return (
         <div className="flex flex-row justify-between items-center bg-[#222222] h-16 rounded-2xl shadow-2xl px-8">
-            <h1 className="text-white text-3xl font-bold">GitFo</h1>
+            <h1 onClick={handleLanding} className="text-white text-3xl font-bold cursor-default">GitFo</h1>
             <div className="flex flex-row gap-10 ">
                 <button onClick={handleLogin} className="bg-[#151515] py-2 px-5 rounded-lg text-white text-lg shadow-inner">Login</button>
                 <button onClick={handleSignUp} className="bg-white py-2 px-5 rounded-lg text-lg">Sign Up</button>
