43ee229f779a7f3de61fe733d9845730561ae166
meetasati07 <mmasati1976@gmail.com>
2026-07-19 14:32:40 +0530

    implemented: login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,16 @@
 import gitHubLogo from '../assets/github-logo.png'
 
 function Login() {
+
+    function handleLogin() {
+        window.location.href = `http://${import.meta.env.VITE_BACKEND_ORIGIN}/auth/login`;
+    }
+
     return (
         <div className="flex flex-col items-center h-100 w-full p-10">
             <div className="flex flex-col gap-10 justify-center items-center  h-100 w-100 rounded-2xl p-10 bg-[#1A1A1A] border-2 border-[#737373]">
                 <h2 className="text-4xl font-bold text-white">Log in to GitFo</h2>
-                <button className="flex gap-4 justify-center h-12 w-72 bg-[#2C2C2C] align-middle rounded-xl text-xl text-white p-2">
+                <button className="flex gap-4 justify-center h-12 w-72 bg-[#2C2C2C] align-middle rounded-xl text-xl text-white p-2" onClick={handleLogin}>
                     <img src={gitHubLogo} alt="" />
                     Continue with GitHub
                 </button>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 import SideBar from "../components/SideBar";
 
 function HomePage() {
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = searchParams.get('token');
+
+        if (token) {
+            localStorage.setItem('access_token', token);
+
+            navigate('/home', { replace: true });
+        }
+    }, [searchParams, navigate]);
+
     return (
         <div className="bg-[#181818] min-h-screen">
             <SideBar />
